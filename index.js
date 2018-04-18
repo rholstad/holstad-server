5b7383c6083e6fa70abfc1df9a3f25e13b8b2939
Ryan Holstad <proximalpod@gmail.com>
2018-04-17 20:26:13 -0400

    initial setup

--- index.js
+++ index.js
@@ -5,21 +5,12 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-
-if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
-}
-
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  databaseURI: process.env.MONGODB_URI
+  cloud: process.env.CLOUD_CODE_MAIN,
+  appId: process.env.APP_ID,
+  masterKey: process.env.MASTER_KEY
+  serverURL: process.env.SERVER_URL
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:

9afbf7c74eb509fd5bd5d8f6202481b06636495e
Ryan Holstad <ryan@motusglobal.com>
2019-08-04 12:51:38 -0400

    pare down response

--- cloud/main.js
+++ cloud/main.js
@@ -6,8 +6,14 @@ Parse.Cloud.define("upcLookup", function(request, response) {
 			upc: upc
 		}
 	}).then(httpResponse => {
-		if (httpResponse.status == 200) {
-			response.success(httpResponse.data);
+		const data = httpResponse.data;
+		if (httpResponse.status == 200 && data.count > 0) {
+			const item = data.items[0];
+			response.success({
+				name: item.title,
+				notes: item.description,
+				barcodeInfo: item
+			});
 		}else {
 			response.error("Not found");
 		}

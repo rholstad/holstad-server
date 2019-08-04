c96fa21b7de04bf1399f74b1ad153b73cff89f9e
Ryan Holstad <ryan@motusglobal.com>
2019-08-04 11:48:02 -0400

    upclookup

--- cloud/main.js
+++ cloud/main.js
@@ -6,7 +6,11 @@ Parse.Cloud.define("upcLookup", function(request, response) {
 			upc: upc
 		}
 	}).then(httpResponse => {
-		response.success(httpResponse);
+		if (httpResponse.status == 200) {
+			response.success(httpResponse.data);
+		}else {
+			response.error("Not found");
+		}
 	},error => {
 		response.error(error);
 	})

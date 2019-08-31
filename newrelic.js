ffa39a9ef3d848599bc6abff89da1857c55df0fd
Ryan Holstad <ryan@motusglobal.com>
2019-08-31 11:54:57 -0400

    new_relic_license_key

--- newrelic.js
+++ newrelic.js
@@ -13,7 +13,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: '8541567d52e613751c89383ee06f4767327003f9',
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
